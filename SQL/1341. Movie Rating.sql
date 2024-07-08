cb4b35d5160e03cdc98f5a84ea94af9b39e713d3
Mishywayu <michellewayua2@gmail.com>
2024-07-08 17:56:43 +0300

    SQL 50

--- SQL/1341. Movie Rating.sql
+++ SQL/1341. Movie Rating.sql
@@ -1,13 +1,18 @@
 # Write your MySQL query statement below
 select name as results
-from (select Users.name, count(movie_id) as movie_count from MovieRating
+from 
+(select Users.name, count(movie_id) as movie_count
+from MovieRating
 left join Users 
 on MovieRating.user_id=Users.user_id
 group by MovieRating.user_id
 order by movie_count desc, name
 limit 1) a
-union select movie_name as results from 
-(SELECT Movies.title as movie_name, avg(MovieRating.rating) as grade 
+union
+select movie_name as results
+from 
+(
+SELECT Movies.title as movie_name, avg(MovieRating.rating) as grade 
 from MovieRating
 left join Movies
 on MovieRating.movie_id=Movies.movie_id
