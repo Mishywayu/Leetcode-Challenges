e832dc87369ceb4d6a5185f1f30033b5152df0c6
Mishywayu <michellewayua2@gmail.com>
2024-09-25 14:34:50 +0300

    Introduction to pandas

---
+++ Introduction to Pandas/2882.py
@@ -0,0 +1,65 @@
+# 2882. Drop Duplicate Rows
+# Easy
+# Companies
+# Hint
+# DataFrame customers
+# +-------------+--------+
+# | Column Name | Type   |
+# +-------------+--------+
+# | customer_id | int    |
+# | name        | object |
+# | email       | object |
+# +-------------+--------+
+# There are some duplicate rows in the DataFrame based on the email column.
+
+# Write a solution to remove these duplicate rows and keep only the first occurrence.
+
+# The result format is in the following example.
+
+ 
+
+# Example 1:
+# Input:
+# +-------------+---------+---------------------+
+# | customer_id | name    | email               |
+# +-------------+---------+---------------------+
+# | 1           | Ella    | emily@example.com   |
+# | 2           | David   | michael@example.com |
+# | 3           | Zachary | sarah@example.com   |
+# | 4           | Alice   | john@example.com    |
+# | 5           | Finn    | john@example.com    |
+# | 6           | Violet  | alice@example.com   |
+# +-------------+---------+---------------------+
+# Output:  
+# +-------------+---------+---------------------+
+# | customer_id | name    | email               |
+# +-------------+---------+---------------------+
+# | 1           | Ella    | emily@example.com   |
+# | 2           | David   | michael@example.com |
+# | 3           | Zachary | sarah@example.com   |
+# | 4           | Alice   | john@example.com    |
+# | 6           | Violet  | alice@example.com   |
+# +-------------+---------+---------------------+
+
+
+# ROUGH
+# import pandas as pd
+# data = {
+#     'id': [1,2,3,4,5,6],
+#     'name': ['Ella', 'David', 'Zachary', 'Alice', 'Finn', 'Violet'],
+#     'email': ['emily@example.com', 'michael@example.com', 'sarah@example.com', 'john@example.com', 'john@example.com', 'alice@example.com']
+# }
+
+# df = pd.DataFrame(data)
+
+# df.drop_duplicates(subset='email', inplace=True)
+
+# print(df)
+
+
+# SOLUTION
+import pandas as pd
+
+def dropDuplicateEmails(customers: pd.DataFrame) -> pd.DataFrame:
+    customers.drop_duplicates(subset='email', inplace=True)
+    return customers
