4f35849515fdd0f92f3667b91df5f7d2a07bd89e
Mishywayu <michellewayua2@gmail.com>
2024-09-26 12:40:52 +0300

    intoduction to pandas

--- Introduction to Pandas/2883.py
+++ Introduction to Pandas/2883.py
@@ -40,14 +40,22 @@
 
 
 # ROUGH
+# import pandas as pd
+
+# data = {
+#     "student_id": [32, 217, 779, 849],
+#     "name": ["Piper", None, "Georgia", "Willow"],
+#     "age" : [5, 19, 20, 14]
+# }
+
+# df = pd.DataFrame(data)
+# df = df.dropna(subset='name')
+# print(df)
+
+
+# SOLUTION
 import pandas as pd
 
-data = {
-    "student_id": [32, 217, 779, 849],
-    "name": ["Piper", None, "Georgia", "Willow"],
-    "age" : [5, 19, 20, 14]
-}
-
-df = pd.DataFrame(data)
-df = df.dropna(inplace=True)
-print(df)
+def dropMissingData(students: pd.DataFrame) -> pd.DataFrame:
+    students = students.dropna(subset='name')
+    return students 
